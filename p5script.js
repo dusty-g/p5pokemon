5894d1a4cebb8437a1dcdd8a2595ecd13acaa478
dusty-g <dustygalindo@gmail.com>
2017-05-02 15:12:27 -0700

    added fireballs

--- p5script.js
+++ p5script.js
@@ -2,18 +2,29 @@
 var count = 0;
 var current_img;
 var current_img_back;
-var images = []
+var images = [];
+var right = true;
+var Y = 0;
+var previousX = 0;
+var direction = true;
+var fireball = false;
+var fire_ball_image;
+var fire_count = 1000;
+var ball_size = 1;
+
 function setup() {
   createCanvas(720, 400);
-  var img1 = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"); 
-  var img1_back = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png");
-  var img2 = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"); 
-  var img2_back = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/5.png");
-  var img3 = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"); 
-  var img3_back = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png");
-  images = [[img1, img1_back],[img2, img2_back],[img3, img3_back]];
-  current_img = img1;
-  current_img_back = img1_back;
+
+  fire_ball_image = loadImage('https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRi6P7vmh-BWnurz8EBrJub1doBde8tYZuPVRqwynhNxkzsFTEq');
+  var charmander = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"); 
+  var charmander_back = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png");
+  var charmeleon = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png"); 
+  var charmeleon_back = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/5.png");
+  var charizard = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png"); 
+  var charizard_back = loadImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png");
+  images = [[charmander, charmander_back],[charmeleon, charmeleon_back],[charizard, charizard_back]];
+  current_img = charmander;
+  current_img_back = charmander_back;
   noCursor(); 
 }
 function keyTyped(){
@@ -25,19 +36,57 @@ function keyTyped(){
             console.log(count)
             console.log(count%3)
         }
+        
 
-        }
+    }
+
 function draw() {
-  // Displays the image at its actual size at point (0,0)
-    background(0)
 
-    
+    background(color('white'));
+    if (fireball){
+        if (right){
+            image(fire_ball_image, fire_count, Y, fire_ball_image.width*ball_size, fire_ball_image.height*ball_size); fire_count++; 
+            if (fire_count > 720){fireball = false;}
+    }
+    else{image(fire_ball_image, fire_count, Y, fire_ball_image.width*ball_size, fire_ball_image.height*ball_size); fire_count--; if (fire_count < 0){fireball = false;}}
+        }
+
+
+
     if (mouseIsPressed){
+        if (direction){fire_count = mouseX + 20; right = true; 
+        }
+        else{fire_count = mouseX - 25; right = false;
+        }
+        // should use a switch statement here
+        if (count % 3 == 0){ball_size = 1}
+        else if (count % 3 == 1){ball_size = 1.5}
+        else if (count % 3 == 2){ball_size = 2.2}
+        console.log(ball_size);
+        Y = mouseY;
+        fireball = true;
+    }
+    
+    if (direction){
+        if (mouseX >= previousX){
         image(current_img_back, mouseX - 50, mouseY - 50)
+        previousX = mouseX
+    } else{
+        direction = false;
+        
+    }
     }else{
+        if (mouseX <= previousX){
         image(current_img, mouseX - 50, mouseY - 50)
+        previousX = mouseX
+    }else{
+        direction = true;
     }
     }
+
+
+
+}
 
 
 
